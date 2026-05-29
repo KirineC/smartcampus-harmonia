5107f34e842f98ab225045f26bc72bbc36698065
Gaïa Lehart <gaia.lehart@gmail.com>
2026-05-29 18:21:34 +0200

    update endroit fichier

--- frontend/src/pages/MonPlanning.jsx
+++ frontend/src/pages/MonPlanning.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../services/api';
-import '../styles/monplanning.css';
+import './MonPlanning.css';
 
 export default function MonPlanning() {
   const [inscriptions, setInscriptions] = useState([]);
